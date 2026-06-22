9cf31f140eae882c71c2c462842a581a90770918
Emilio Cimino <emilio.cimino@outlook.it>
2026-06-22 15:17:22 +0200

    Dev (#11)
    * hotfix garage connection
    
    * hotfix garage connection

--- src/server/storage_handlers/garage.py
+++ src/server/storage_handlers/garage.py
@@ -28,6 +28,9 @@ client = minio.Minio(
 
 
 def remote_storage_connect_and_sync() -> bool:
+    if GARAGE_URL is None:
+        return False
+
     if bucket_exists():
         try:
             get_available_models()
